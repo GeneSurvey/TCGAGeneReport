d164ba71efb7cac75b628d38f449a7e42e01cf7d
Tod Casasent <tdcasasent@mdanderson.org>
2016-03-15 12:17:50 -0500

    Updated license header

--- R/TCGAGeneReport.R
+++ R/TCGAGeneReport.R
@@ -1,4 +1,4 @@
-#TCGAGeneReport Copyright 2014, 2015 University of Texas MD Anderson Cancer Center
+#TCGAGeneReport Copyright 2014, 2015, 2016 University of Texas MD Anderson Cancer Center
 #
 #This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
 #
@@ -35,7 +35,7 @@ padChromosomeName <- function(theChr)
 initGeneReport <- function(theParameters="-Xms2400m")
 {
 	stopifnot(is.character(theParameters))
-	message("TCGAGeneReport 2015-12-28-1522")
+	message("TCGAGeneReport 2016-03-11-1300")
 	message("note: parameters must be in terms of 'm' or .jinit will fail on Linux")
 	message("In testing, -Xmx42000m worked, while larger values caused core exceptions")
 	myJavaJars <- file.path(
